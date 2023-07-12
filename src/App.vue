8235f4ac41634dc9334d4961e3668e754d0198fa
Victor Dias <victordiaspereira53@gmail.com>
2023-07-12 20:33:00 -0300

    Copia Linktre Victor Dias

---
+++ src/App.vue
@@ -0,0 +1,72 @@
+<script setup>
+import Item from "./components/item.vue";
+</script>
+
+<script>
+import Dados from "./Dados/services.json";
+export default {
+  data() {
+    return {
+      servicos: null,
+    };
+  },
+  mounted() {
+    this.servicos = Dados;
+  },
+};
+</script>
+
+<template>
+  <div class="logo">
+    <img alt="Vue logo" src="src/Dados/img/profile-pic (1).png" /><span
+      id="nome"
+      >Victor Dias Pereira</span
+    >
+  </div>
+  <div class="items">
+    <Item v-for="(servico, index) in servicos" :key="index" :item="servico" />
+  </div>
+</template>
+
+<style scoped>
+.logo {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+
+.logo img {
+  width: 200px;
+  height: 200px;
+}
+.items {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0 auto;
+}
+
+#nome {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 10px;
+  font-size: x-large;
+  color: white;
+}
+
+@media (max-width: 768px) {
+  .items {
+    flex-wrap: wrap;
+  }
+}
+
+@media (max-width: 768px) {
+  .logo {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+}
+</style>
---
+++ src/components/item.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <a :href="item.href" target="_blank">
+      <img :src="item.img" alt="Icone" />
+      <span>
+        {{ item.titulo }}
+      </span>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["item"],
+};
+</script>
+
+<style scoped>
+
+div {
+  display: flex;
+  background-color: white;
+  color: black;
+  flex-direction: column;
+  padding: 10px;
+  text-align: center;
+  width: 150px;
+  margin: 20px 30px 20px 30px;
+  border-radius: 20px;
+}
+
+img {
+  width: 100px;
+}
+
+span {
+  font-weight: 900;
+}
+a {
+  color: black;
+  text-decoration: none;
+  text-transform: uppercase;
+}
+a:hover {
+  opacity: 0.5;
+}
+</style>
